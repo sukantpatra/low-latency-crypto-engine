6e2b0715af10bdeb4c44d6cd2e7a9515080e59be
SukantPatra <sukantpatra070@gmail.com>
2026-08-17 00:00:42 +0530

    chore: untrack build folder and update gitignore

--- build/_deps/simdjson-build/simdjson-props.cmake
+++
@@ -1,5 +0,0 @@
-target_include_directories("${target}" ${public} [==[$<BUILD_INTERFACE:/Users/sukantpatra/HFT_Basics_Mac/build/_deps/simdjson-src/include>]==] ${private} [==[$<BUILD_INTERFACE:/Users/sukantpatra/HFT_Basics_Mac/build/_deps/simdjson-src/src>]==])
-target_compile_features("${target}" ${public} [==[cxx_std_11]==])
-target_compile_options("${target}" ${private} [==[$<$<CONFIG:DEBUG>:-Og>]==])
-target_link_libraries("${target}" ${public} [==[Threads::Threads]==])
-target_compile_definitions("${target}" ${public} [==[SIMDJSON_THREADS_ENABLED=1]==])
